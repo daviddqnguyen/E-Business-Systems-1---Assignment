c714eef283bfeabade74c90d1aa38179ba37187e
David Nguyen <s3422643@student.rmit.edu.au>
2019-10-11 10:21:14 +1100

    Fixed syntax of login.php script

--- scripts/login.php
+++ scripts/login.php
@@ -12,7 +12,7 @@
         }
     }
 
-    if (! (isset($_SESSION['logged_in'])) && $_SESSION['logged_in'] == 'true' ) {
+    if (! (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] == 'true' ) ) {
         echo '
         <html>
             <head>
