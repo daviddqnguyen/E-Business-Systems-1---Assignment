35959325c13d77b2d0924a8ab9931c76d10bb66b
connortm <55578486+connortm@users.noreply.github.com>
2019-10-11 11:55:07 +1100

    Added SEO meta tags

--- contact-us.php
+++ contact-us.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="Contact us">
     <script src="http://code.jquery.com/jquery-3.3.1.min.js"></script>
     <script src="scripts/simplecartjs/simpleCart.js"></script>
     <script src="scripts/simplecartjs-config.js"></script>
